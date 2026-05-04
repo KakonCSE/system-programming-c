8b1124f92738e6684b8c0751ba43f88de8a1d30d
kakon <kakon.ray@polygontech.xyz>
2026-05-04 23:28:53 +0600

    Add temperature print using float variable

---
+++ print-function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int marks = 100;
+    int price = 50;
+
+    float temparature = 30.5;
+    printf("Temp = %f", temparature);
+    //printf("Marks = %d, price = %d", marks, price);
+    return 0;
+
+}
